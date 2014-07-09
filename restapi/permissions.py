727eedae304ba0e0e8945c6572b5ae805f638678
celin.chassot <celin.chassot@edu.hefr.ch>
2014-07-09 13:40:45 +0200

    comments

--- restapi/permissions.py
+++ restapi/permissions.py
@@ -6,13 +6,8 @@ from rest_framework.authentication import SessionAuthentication
 class UnsafeSessionAuthentication(SessionAuthentication):
 
     def authenticate(self, request):
-        http_request = request._request
-        user = getattr(http_request, 'user', None)
 
-        if not user or not user.is_active:
-           return None
-
-        return (user, None)
+        return None
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
